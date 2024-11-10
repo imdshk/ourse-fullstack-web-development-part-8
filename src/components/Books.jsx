314b617cda2d6752881df3860f1e5ed48053dbf0
imdshk <83509028+imdshk@users.noreply.github.com>
2024-11-10 16:28:03 +0200

    updates

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,45 +5,70 @@ import { useQuery, useSubscription } from "@apollo/client"
 import { updateCache } from "../App"
 
 const Books = ({ books }) => {
-  const [filteredBooks, setfilteredBooks] = useState(books)
-  const [selectedGenre, setSelectedGenre] = useState('')
+  // const [filteredBooks, setfilteredBooks] = useState(books)
+  // const [selectedGenre, setSelectedGenre] = useState('')
 
-  let genres = [] 
-  books.forEach(book => {
-    genres = genres.concat(book.genres)
-  })
+  // let genres = [] 
+  // books.forEach(book => {
+  //   genres = genres.concat(book.genres)
+  // })
 
-  const uniqueGenres = [ ...new Set(genres)]
+  // const uniqueGenres = [ ...new Set(genres)]
 
-  const onClick = (genre) => {
-    setSelectedGenre(genre)
-  }
+  // const onClick = (genre) => {
+  //   setSelectedGenre(genre)
+  // }
 
-  const result_filteredBooks = useQuery(FIND_BOOKS, {
-    variables: { genreToSearch: selectedGenre }
-  }, [selectedGenre])
+  // const result_filteredBooks = useQuery(FIND_BOOKS, {
+  //   variables: { genreToSearch: selectedGenre }
+  // }, [selectedGenre])
+  
+  // useEffect(() => {  
+  //   if (result_filteredBooks.data) {  
+  //     setfilteredBooks(result_filteredBooks.data.allBooks) 
+  //   }  
+  // }, [result_filteredBooks.data]) 
+
+  // useSubscription(BOOK_ADDED, {
+  //   onData: ({ data, client }) => {
+  //     const addedBook = data.data.bookAdded
+  //     updateCache(client.cache, { query: ALL_BOOKS }, addedBook)
+  //     window.alert(`${addedBook.title} added`)
+  //     client.cache.updateQuery(
+  //       {query: ALL_BOOKS}, ({allBooks}) => {
+  //         return {
+  //           allBooks: allBooks.concat(addedBook)
+  //         }
+  //       }
+  //     )
+  //   }
+  // })
+  const [filteredBooks, setFilteredBooks] = useState(books)  
+  const [selectedGenre, setSelectedGenre] = useState('')  
+  
+  const { data: subscriptionData } = useSubscription(BOOK_ADDED);  
+  const { loading, error, data } = useQuery(FIND_BOOKS, {  
+    variables: { genreToSearch: selectedGenre },  
+  });  
   
   useEffect(() => {  
-    if (result_filteredBooks.data) {  
-      setfilteredBooks(result_filteredBooks.data.allBooks) 
+    if (subscriptionData?.bookAdded) {  
+      setFilteredBooks((prevBooks) => [...prevBooks, subscriptionData.bookAdded]);  
     }  
-  }, [result_filteredBooks.data]) 
-
-  useSubscription(BOOK_ADDED, {
-    onData: ({ data, client }) => {
-      const addedBook = data.data.bookAdded
-      updateCache(client.cache, { query: ALL_BOOKS }, addedBook)
-      window.alert(`${addedBook.title} added`)
-      client.cache.updateQuery(
-        {query: ALL_BOOKS}, ({allBooks}) => {
-          return {
-            allBooks: allBooks.concat(addedBook)
-          }
-        }
-      )
-    }
-  })
+    else if (!loading && data) {  
+      setFilteredBooks(data.allBooks);  
+    }  
+  }, [subscriptionData, loading, data]);  
   
+  const genres = filteredBooks.reduce((allGenres, book) => {  
+    return allGenres.concat(book.genres);  
+  }, []);  
+    
+  const uniqueGenres = [...new Set(genres)];  
+  
+  const onClick = (genre) => {  
+    setSelectedGenre(genre);  
+  }
 
   return (
     <div>
